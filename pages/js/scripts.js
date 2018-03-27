a818d789ba16ed25833e9563bb97699eb7097aac
TTPixelNet <37836769+TTPixelNet@users.noreply.github.com>
2018-03-27 14:35:00 +0200

    Update scripts.js

--- pages/js/scripts.js
+++ pages/js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $.firefly();
 
     $.ajax({
-        url: "js/player_count.txt",
+        url: "pages/js/player_count.txt",
         success: function(result) {
             $("h1").html(result + " players are <span class='online'>online</span>");
         },
